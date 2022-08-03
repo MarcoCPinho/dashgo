2369ce1f20edfbccd87432d90c3eb47ad7f74db6
Marco Carvalho de Pinho <marcocpinho@hotmail.com>
2022-08-03 10:28:57 -0300

    Update Logo.tsx

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,7 +1,12 @@
 import { Text } from "@chakra-ui/react";
 
 const Logo = () => (
-  <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+  <Text
+    fontSize={["2xl", "3xl"]}
+    fontWeight="bold"
+    letterSpacing="tight"
+    w="64"
+  >
     dashogo
     <Text as="span" marginLeft="1" color="pink.500">
       .
