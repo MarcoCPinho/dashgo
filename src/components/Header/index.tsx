0f4b2d07de03f1c4bd56d8d05fa79ae28ed3fa2c
Marco Carvalho de Pinho <marcocpinho@hotmail.com>
2022-08-05 07:05:57 -0300

    aulas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
 import Logo from "./Logo";
 import NotificationsNav from "./NotificationsNav";
 import Profile from "./Profile";
@@ -6,6 +8,7 @@ import SearchBox from "./SearchBox";
 
 const Header = () => {
   const isWideVersion = useBreakpointValue({ base: false, lg: true });
+  const { onOpen } = useSidebarDrawer();
 
   return (
     <Flex
@@ -18,6 +21,17 @@ const Header = () => {
       paddingX="4"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        ></IconButton>
+      )}
+
       <Logo />
 
       {isWideVersion && <SearchBox />}
